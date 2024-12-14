3c8e2b3be9b92f42ed809c98bc60775b9fd4aad2
FTC Team 21180 Error418 <108445260+EBError418@users.noreply.github.com>
2024-12-14 10:49:01 -0600

    add 3 specimen

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRightHangingWall.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRightHangingWall.java
@@ -7,7 +7,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
-@Autonomous(name=" A - specimens from wall", group="Concept")
+@Autonomous(name=" A - 4 specimens", group="Concept")
 public class AutoRightHangingWall extends AutoRightHanging2 {
     @Override
     public void setFloor() {
