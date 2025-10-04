b2c2d27071383641b7075693d3dde55454a9a15d
Kartikeya <kcols121@proton.me>
2025-10-04 14:52:57 -0500

    Semi Built RR Path (#15)
    * simple test
    
    * testing
    
    * Auto + Code for converting area of artifact bounding box into half mats.
    
    * Fixed pixels to halfmats function. Fixed robot location function.
    
    * Semi-Built RR Path
    
    ---------
    
    Co-authored-by: FTC Team 21180 Error418 <108445260+EBError418@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -14,11 +14,12 @@ public class AutoTest extends LinearOpMode {
     public int leftOrRight = 1;
     Pose2d newStartPose;
     Vector2d moveForward = new Vector2d(2 * Params.HALF_MAT, 0);
-
     Vector2d moveToBall;
 
-    public void setRobotLocation(int side) {
-        leftOrRight = side;  // side should be either -1 for left or 1 for right
+    Vector2d shootPos = new Vector2d(Params.HALF_MAT, Params.HALF_MAT);
+
+    public void setRobotLocation() {
+        leftOrRight = 1;  // side should be either -1 for left or 1 for right
     }
 
     private void setStartPoses(int leftRight) {
@@ -36,11 +37,10 @@ public class AutoTest extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        setRobotLocation(1); // -1 for left or 1 for right
-
+        setRobotLocation();
         setStartPoses(leftOrRight);
 
-        driveToBall(pixelsToHalfmats(100)); // replace 100 with area of bounding box from LL
+        driveToBall(pixelsToHalfmats(15590)); // replace 100 with area of bounding box from LL
 
         drive = new MecanumDrive(hardwareMap, newStartPose);
         waitForStart();   // <-- REQUIRED
@@ -50,10 +50,44 @@ public class AutoTest extends LinearOpMode {
     }
 
     private void auto() {
+//        Actions.runBlocking(
+//                drive.actionBuilder(newStartPose)
+//                        .strafeTo(moveToBall)
+//                        .build()
+//        );
+
         Actions.runBlocking(
                 drive.actionBuilder(newStartPose)
-                        .strafeTo(moveToBall)
+                        .strafeToLinearHeading(shootPos, Math.toRadians(45))
                         .build()
         );
+
+        // Shoot balls code here
+
+
+        // End shoot balls code
+
+        Actions.runBlocking(
+                drive.actionBuilder(newStartPose)
+                        .strafeToLinearHeading(new Vector2d(-4 * Params.HALF_MAT, 2 * Params.HALF_MAT), Math.toRadians(90))
+                        .build()
+        );
+
+        Actions.runBlocking(
+                drive.actionBuilder(newStartPose)
+                        .strafeTo(new Vector2d(0, 2 * Params.HALF_MAT))
+                        .build()
+        );
+
+        Actions.runBlocking(
+                drive.actionBuilder(newStartPose)
+                        .strafeToLinearHeading(shootPos, Math.toRadians(45))
+                        .build()
+        );
+
+        // Shoot balls code here
+
+
+        // End shoot balls code
     }
 }
