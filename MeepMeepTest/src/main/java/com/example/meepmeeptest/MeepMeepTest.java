baaa724f803183de0c3d3e0c5eb870eae01476dc
FTC Team 21180 Error418 <108445260+EBError418@users.noreply.github.com>
2025-10-08 21:52:27 -0500

    basic auto path
    made basic auto path capable of two cycles

--- MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTest.java
+++ MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTest.java
@@ -11,31 +11,23 @@ public class MeepMeepTest {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-        Pose2d newStartPose = new Pose2d((-6 * Params.HALF_MAT + Params.CHASSIS_LENGTH / 2),(- Params.CHASSIS_HALF_WIDTH),Math.toRadians(179.9998));
+        int leftRight = 1;
+        Pose2d newStartPose;
+        newStartPose = new Pose2d((-6 * Params.HALF_MAT + Params.CHASSIS_LENGTH / 2),(leftRight * Params.CHASSIS_HALF_WIDTH),0.0);
+        Vector2d moveForward = new Vector2d(2 * Params.HALF_MAT, 0);
+        Vector2d moveToBall;
 
-        Vector2d firstHighChamberPos = new Vector2d(-3.5 * Params.HALF_MAT, newStartPose.getY());
-
-        Vector2d clearSubForSamples = new Vector2d(- 2.7 * Params.HALF_MAT, - 3.0 * Params.HALF_MAT);
-        Vector2d behindSamplePos = new Vector2d(- 0.5 * Params.HALF_MAT, - 3.8 * Params.HALF_MAT);
-        Vector2d obsZone = new Vector2d(- 3.3 * Params.HALF_MAT, - 3.8 * Params.HALF_MAT);
+        Pose2d shootPos = new Pose2d(Params.HALF_MAT, Params.HALF_MAT, Math.toRadians(45));
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(firstHighChamberPos, Math.toRadians(179.9998)))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(newStartPose)
                         //.afterTime(0.3, new armToPickUpPos()) // lower arm during spline moving
-                        .splineToConstantHeading(clearSubForSamples, Math.toRadians(0))
-                        .splineToConstantHeading(behindSamplePos, Math.toRadians(-110))
-                        //.strafeTo(new Vector2d(behindSamplePos.getX() - 0.5 * Params.HALF_MAT, clearSubForSamples.getY() - 0.5 * Params.HALF_MAT))
-                        //.strafeTo(behindSamplePos)
-                        .strafeTo(new Vector2d(obsZone.getX() - 1.2 * Params.HALF_MAT, behindSamplePos.getY())) // moving to obs zone
-                        .splineToConstantHeading(new Vector2d(behindSamplePos.getX(), behindSamplePos.getY()- 0.99 * Params.HALF_MAT), Math.toRadians(-135))
-                        //.strafeTo(new Vector2d(behindSamplePos.getX(), - 5 * Params.HALF_MAT))//move behind 2nd sample
-                        .strafeTo(new Vector2d(obsZone.getX() - 1.2 * Params.HALF_MAT, - 5 * Params.HALF_MAT)) // moving to obs zone
-                        .strafeTo(new Vector2d(behindSamplePos.getX(), - 5.1 * Params.HALF_MAT))//move back
-                        .strafeTo(new Vector2d(behindSamplePos.getX(), - 6 * Params.HALF_MAT + Params.CHASSIS_HALF_WIDTH))//move behind 3rd sample
-                        .strafeTo(new Vector2d(obsZone.getX() - 1.2 * Params.HALF_MAT, - 6 * Params.HALF_MAT + Params.CHASSIS_HALF_WIDTH)) // moving to obs zone
-                        //.afterTime(0.01, new intakeAct(0,0,intake.FINGER_OPEN)) // drop off first sample after 4 inch strafe
+                        .strafeToLinearHeading(new Vector2d(shootPos.getX(), shootPos.getY()), shootPos.getHeading())
+                        .strafeToLinearHeading(new Vector2d(-3 * Params.HALF_MAT,3 * Params.HALF_MAT),Math.toRadians(90))
+                        .strafeTo(new Vector2d(-3 * Params.HALF_MAT, 3.75 * Params.HALF_MAT))
+                        .splineToLinearHeading(shootPos, Math.toRadians(0))
                         .build());
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -3,12 +3,13 @@ package org.firstinspires.ftc.teamcode;
 import java.io.*;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Rotation2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name="AutoTest", group="Concept")
+@Autonomous(name="ojf", group="Concept")
 public class AutoTest extends LinearOpMode {
     private MecanumDrive drive;
     public int leftOrRight = 1;
@@ -16,7 +17,7 @@ public class AutoTest extends LinearOpMode {
     Vector2d moveForward = new Vector2d(2 * Params.HALF_MAT, 0);
     Vector2d moveToBall;
 
-    Vector2d shootPos = new Vector2d(Params.HALF_MAT, Params.HALF_MAT);
+    Pose2d shootPos = new Pose2d(Params.HALF_MAT, Params.HALF_MAT, Math.toRadians(45));
 
     public void setRobotLocation() {
         leftOrRight = 1;  // side should be either -1 for left or 1 for right
@@ -24,7 +25,7 @@ public class AutoTest extends LinearOpMode {
 
     private void setStartPoses(int leftRight) {
         // road runner variables
-        newStartPose = new Pose2d((-6 * Params.HALF_MAT + Params.CHASSIS_LENGTH / 2),(-leftRight * Params.CHASSIS_HALF_WIDTH),0.0);
+        newStartPose = new Pose2d((-6 * Params.HALF_MAT + Params.CHASSIS_LENGTH / 2),(leftRight * Params.CHASSIS_HALF_WIDTH),0.0);
     }
 
     public void driveToBall(double distance) { // distance should be in half mats
@@ -58,32 +59,40 @@ public class AutoTest extends LinearOpMode {
 
         Actions.runBlocking(
                 drive.actionBuilder(newStartPose)
-                        .strafeToLinearHeading(shootPos, Math.toRadians(45))
+                        .strafeToLinearHeading(shootPos.position, shootPos.heading)
                         .build()
         );
-
+        telemetry.addLine("moved to shoot pos 1");
+        sleep(1000);
         // Shoot balls code here
 
 
         // End shoot balls code
 
         Actions.runBlocking(
-                drive.actionBuilder(newStartPose)
-                        .strafeToLinearHeading(new Vector2d(-4 * Params.HALF_MAT, 2 * Params.HALF_MAT), Math.toRadians(90))
+                drive.actionBuilder(drive.localizer.getPose())
+                        .strafeToLinearHeading(new Vector2d(-3 * Params.HALF_MAT,3 * Params.HALF_MAT),Math.toRadians(90))
                         .build()
         );
+        telemetry.addLine("Moved to pick up pos 1");
+        sleep(1000);
 
         Actions.runBlocking(
-                drive.actionBuilder(newStartPose)
-                        .strafeTo(new Vector2d(0, 2 * Params.HALF_MAT))
+                drive.actionBuilder(drive.localizer.getPose())
+                        .strafeTo(new Vector2d(-3 * Params.HALF_MAT, 3.75 * Params.HALF_MAT))
                         .build()
         );
+        telemetry.addLine("Forward to pick up 1");
+        sleep(1000);
 
         Actions.runBlocking(
-                drive.actionBuilder(newStartPose)
-                        .strafeToLinearHeading(shootPos, Math.toRadians(45))
+                drive.actionBuilder(drive.localizer.getPose())
+                        .strafeTo(new Vector2d(-3 * Params.HALF_MAT, 3 * Params.HALF_MAT))
+                        .strafeToLinearHeading(shootPos.position, shootPos.heading)
                         .build()
         );
+        telemetry.addLine("Moved to shoot pos 2");
+        sleep(1000);
 
         // Shoot balls code here
 
