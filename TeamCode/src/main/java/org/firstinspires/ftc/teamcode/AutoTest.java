9b90fabb9a313aab7d06d8786ef697eb24890efc
Anson Li <130667068+28lian@users.noreply.github.com>
2025-10-11 14:13:52 -0500

    Started working on merging Limelight into Autotest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -1,126 +1,115 @@
 package org.firstinspires.ftc.teamcode;
 
-import java.io.*;
-
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name="Auto2026", group="Concept")
+@Autonomous(name = "Auto2026", group = "Concept")
 public class AutoTest extends LinearOpMode {
     private MecanumDrive drive;
-    private Colored patterndetector;
-    public int leftOrRight = 1;
-    Pose2d newStartPose;
-    Vector2d moveToBall;
+    private Colored patternDetector; // Limelight detector
 
-    Pose2d shootPos = new Pose2d(Params.HALF_MAT, Params.HALF_MAT, Math.toRadians(45));
+    private int leftOrRight = 1;
+    private Pose2d newStartPose;
+    private Vector2d moveToBall;
 
-    public void setRobotLocation() {
-        leftOrRight = 1;  // side should be either -1 for left or 1 for right
-    }
-
-    private void setStartPoses(int leftRight) {
-        // road runner variables
-        newStartPose = new Pose2d((-6 * Params.HALF_MAT + Params.CHASSIS_LENGTH / 2),(leftRight * Params.CHASSIS_HALF_WIDTH),0.0);
-    }
-
-    public void driveToBall(double distance) { // distance should be in half mats
-        moveToBall = new Vector2d(newStartPose.position.x + distance, 0);
-    }
-
-    public double pixelsToHalfmats(double pixels) { // pixels = area of bounding box
-        return Math.sqrt(62320.0 / pixels) / 2; // returns half mats
-    }
+    private Pose2d shootPos = new Pose2d(Params.HALF_MAT, Params.HALF_MAT, Math.toRadians(45));
 
     @Override
     public void runOpMode() {
+        // Initialize drive and vision system
+        patternDetector = new Colored(hardwareMap);
         setRobotLocation();
         setStartPoses(leftOrRight);
-
-        driveToBall(pixelsToHalfmats(15590)); // replace with area of bounding box from LL
-
         drive = new MecanumDrive(hardwareMap, newStartPose);
-        waitForStart();   // <-- REQUIRED
+
+        telemetry.addLine("Initialized. Waiting for start...");
+        telemetry.update();
+
+        waitForStart();
+
         if (opModeIsActive()) {
-            auto();
+            // Try a few times to get a valid color reading
+            double detectedColor = 0;
+            for (int i = 0; i < 30 && opModeIsActive(); i++) { // up to ~0.3s
+                detectedColor = patternDetector.returnId();
+                telemetry.addData("Detected Color", detectedColor);
+                telemetry.update();
+                if (detectedColor != 0) break;
+                sleep(10);
+            }
+
+            auto(detectedColor);
         }
     }
 
-    private void auto() {
+    private void setRobotLocation() {
+        leftOrRight = 1;
+    }
+
+    private void setStartPoses(int leftRight) {
+        newStartPose = new Pose2d(
+                (-6 * Params.HALF_MAT + Params.CHASSIS_LENGTH / 2),
+                (leftRight * Params.CHASSIS_HALF_WIDTH),
+                0.0
+        );
+    }
+
+//    private void driveToBall(double distance) {
+//        moveToBall = new Vector2d(newStartPose.position.x + distance, 0);
+//    }
+
+    private double pixelsToHalfmats(double pixels) {
+        return Math.sqrt(62320.0 / pixels) / 2;
+    }
+
+    private void auto(double detectedPattern) {
         double distanceToShootPos = Math.hypot(
                 shootPos.position.x - newStartPose.position.x,
                 shootPos.position.y - newStartPose.position.y
         );
 
+        telemetry.addData("Running Auto", true);
+        telemetry.addData("Pattern", detectedPattern);
+        telemetry.update();
+
         Actions.runBlocking(
                 drive.actionBuilder(newStartPose)
-                        // Move to shooting position
                         .strafeToLinearHeading(shootPos.position, shootPos.heading)
-                        // Shoot
                         .afterDisp(distanceToShootPos, () -> {
-                            sortArtifacts(22);
-
+                            sortArtifacts((int) detectedPattern);
                             shootArtifacts();
                         })
-                        // First move to artifacts
-                        .strafeToLinearHeading(new Vector2d(-3 * Params.HALF_MAT,3 * Params.HALF_MAT),Math.toRadians(90))
-                        // Move forward to collect artifacts
+                        .strafeToLinearHeading(new Vector2d(-3 * Params.HALF_MAT, 3 * Params.HALF_MAT), Math.toRadians(90))
                         .strafeTo(new Vector2d(-3 * Params.HALF_MAT, 3.75 * Params.HALF_MAT))
-                        // Move a little back
                         .strafeTo(new Vector2d(-3 * Params.HALF_MAT, 3 * Params.HALF_MAT))
-                        // Move to shooting position
                         .strafeToLinearHeading(shootPos.position, shootPos.heading)
-                        // Shoot
-                        .afterDisp(distanceToShootPos, () -> {
-                            sortArtifacts(23);
-
-                            shootArtifacts();
-                        })
-                        // Second move to artifacts
-                        .strafeToLinearHeading(new Vector2d(-1 * Params.HALF_MAT, 3 * Params.HALF_MAT), Math.toRadians(90))
-                        // Move forward to collect artifacts
-                        .strafeTo(new Vector2d(-1 * Params.HALF_MAT, 3.75 * Params.HALF_MAT))
-                        // Move a little back
-                        .strafeTo(new Vector2d(-1 * Params.HALF_MAT, 3 * Params.HALF_MAT))
-                        // Move to shooting position
-                        .strafeToLinearHeading(shootPos.position, shootPos.heading)
-                        // Shoot
-                        .afterDisp(distanceToShootPos, () -> {
-                            sortArtifacts(21);
-
-                            shootArtifacts();
-                        })
-                        // Empty artifacts
-                        .strafeToLinearHeading(new Vector2d(0, 4 * Params.HALF_MAT), Math.toRadians(90))
-
                         .build()
         );
     }
 
     private void shootArtifacts() {
-        // Shoot balls code here
+        telemetry.addLine("Shooting...");
+        telemetry.update();
     }
 
     private void sortArtifacts(int pattern) {
-        // Sort balls code here
-
         switch (pattern) {
             case 21:
-                // Pattern 1 code here
+                telemetry.addLine("Sorting: GPP");
                 break;
             case 22:
-                // Pattern 2 code here
+                telemetry.addLine("Sorting: PGP");
                 break;
             case 23:
-                // Pattern 3 code here
+                telemetry.addLine("Sorting: PPG");
                 break;
             default:
-                // Default case code here
+                telemetry.addLine("Sorting: Unknown");
                 break;
         }
+        telemetry.update();
     }
-
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Colored.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Colored.java
@@ -1,57 +1,52 @@
 package org.firstinspires.ftc.teamcode;
 import java.util.ArrayList;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
-@Autonomous(name="Colored", group="Concept")
-public class Colored extends LinearOpMode {
+@Config
+public final class Colored{
     Limelight3A limelight;
     String pattern = "none";
 
-    @Override
-    public void runOpMode() {
+    public Colored(HardwareMap hardwareMap) {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
         limelight.start(); // This tells Limelight to start looking!
-        waitForStart();   // <-- REQUIRED
         limelight.pipelineSwitch(2);
-        while (opModeIsActive()) {
-            // receive chunked results
-            ArrayList<ArrayList<Double>> chunkedResults = chunkResult();
-            if (chunkedResults.size() > 0) {
-                for (ArrayList<Double> result : chunkedResults) {
-                    if (result.size() == 5) {
-                        double colorCode = result.get(0);
-                        double x = result.get(1);
-                        double y = result.get(2);
-                        double w = result.get(3);
-                        double h = result.get(4);
-//                        if (colorCode == 1) {
-//                            telemetry.addData("Green Artifact", "x: %.1f, y: %.1f, w: %.1f, h: %.1f, area: %.1f", x, y, w, h, w*h);
-//                        } else if (colorCode == 2) {
-//                            telemetry.addData("Purple Artifact", "x: %.1f, y: %.1f, w: %.1f, h: %.1f, area: %.1f", x, y, w, h, w*h);
-//                        } else
-                        if (colorCode == 20){
-                            telemetry.addData("Blue Goal", "x: %.1f, y: %.1f, w: %.1f, h: %.1f, area: %.1f", x, y, w, h, w*h);
-                        } else if (colorCode == 21){
-                            telemetry.addData("GPP Pattern", "x: %.1f, y: %.1f, w: %.1f, h: %.1f, area: %.1f", x, y, w, h, w*h);
-                        } else if (colorCode == 22){
-                            telemetry.addData("PGP Pattern", "x: %.1f, y: %.1f, w: %.1f, h: %.1f, area: %.1f", x, y, w, h, w*h);
-                        } else if (colorCode == 23){
-                            telemetry.addData("PPG Pattern", "x: %.1f, y: %.1f, w: %.1f, h: %.1f, area: %.1f", x, y, w, h, w*h);
-                        } else if (colorCode == 24){
-                            telemetry.addData("Red Goal", "x: %.1f, y: %.1f, w: %.1f, h: %.1f, area: %.1f", x, y, w, h, w*h);
-                        }
+        // receive chunked results
+
+
+
+    }
+    public double returnId() {
+        ArrayList<ArrayList<Double>> chunkedResults = chunkResult();
+        if (!chunkedResults.isEmpty()) {
+            for (ArrayList<Double> result : chunkedResults) {
+                if (result.size() == 5) {
+                    double colorCode = result.get(0);
+                    double x = result.get(1);
+                    double y = result.get(2);
+                    double w = result.get(3);
+                    double h = result.get(4);
+//                       if (colorCode == 1) {
+//                           telemetry.addData("Green Artifact", "x: %.1f, y: %.1f, w: %.1f, h: %.1f, area: %.1f", x, y, w, h, w*h);
+//                       } else if (colorCode == 2) {
+//                           telemetry.addData("Purple Artifact", "x: %.1f, y: %.1f, w: %.1f, h: %.1f, area: %.1f", x, y, w, h, w*h);
+//                       } else
+                    if (colorCode == 21 || colorCode == 22 || colorCode == 23) {
+                        return colorCode;
                     }
                 }
             }
-            telemetry.update();
         }
+        return 0;
     }
-    public ArrayList<ArrayList<Double>> chunkResult() {
+    private ArrayList<ArrayList<Double>> chunkResult() {
         LLResult result = limelight.getLatestResult();
         double[] pythonOutputs = result.getPythonOutput();
         // pythonOutputs is a long array where each group of 5 values represents one detected object
