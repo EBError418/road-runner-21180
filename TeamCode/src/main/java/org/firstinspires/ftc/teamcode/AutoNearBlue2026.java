31ecfe52fd90fdd5eafd5110937de855c4d60726
Anson Li <130667068+28lian@users.noreply.github.com>
2025-11-03 22:04:21 -0600

    Commented and cleaned up code. Added clear TODOs regarding saving time and code efficiency, should look at this next meeting. Also fixed a few crucial polarity issues (that made it only accurate for AutoNearBlue and not AutoNearRed). Hopefully, AutoNearRed can be functional with a simple change of the variable leftOrRight from 1 to -1 now. After we test out AutoNearRed and succeeding, we can then simply change some values to create an AutoFarRed and AutoFarBlue.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoNearBlue2026.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoNearBlue2026.java
@@ -1,31 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.widget.VectorEnabledTintResources;
-
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.InstantAction;
 import com.acmerobotics.roadrunner.VelConstraint;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.ParallelAction;
-import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Autonomous(name = "Auto Near 2026", group = "Concept")
-public class AutoNear2026 extends LinearOpMode {
+@Autonomous(name = "Auto Near Blue 2026", group = "Concept")
+public class AutoNearBlue2026 extends LinearOpMode {
     // get the software-hardware links ready
     private MecanumDrive drive;
     private intakeUnit2026 motors;
     private Colored patternDetector;
 
-    private int leftOrRight = 1; // 1 for blue, -1 for red
+    private final int leftOrRight = 1; // 1 for blue, -1 for red
     private double detectedPattern; // limelight detected pattern
 
-    private Pose2d StartPose; // where the robot is placed at the start
     private Vector2d shootPos; // where the robot should shoot
     private double shootHeading; //the direction the robot shoot in
 
@@ -36,7 +30,8 @@ public class AutoNear2026 extends LinearOpMode {
         motors = new intakeUnit2026(hardwareMap, "launcher", "intake", "triggerServo");
 
         // set the starting position at (6 HALFMATS - HALF OF ROBOT LENGTH, HALF OF ROBOT WIDTH [sign depends on sign]) and with heading in reverse
-        StartPose = new Pose2d(
+        // where the robot is placed at the start
+        Pose2d startPose = new Pose2d(
                 (6 * Params.HALF_MAT - Params.CHASSIS_HALF_LENGTH),
                 (leftOrRight * Params.CHASSIS_HALF_WIDTH), Math.toRadians(180.0)
         );
@@ -44,19 +39,13 @@ public class AutoNear2026 extends LinearOpMode {
         // define shoot position at (HALF_MAT, HALF_MAT), (HALF_MAT, -HALF_MAT), calculate shoot angle based on location x, y
         double shootPosX = 1 * Params.HALF_MAT;
         double shootPosY = leftOrRight * Params.HALF_MAT;
-        shootHeading = Math.toRadians(180) + Math.atan2(6 * Params.HALF_MAT - shootPosY, 6 * Params.HALF_MAT - shootPosX);
+        // made the following polarity change to shootHeading calculation
+        shootHeading = Math.toRadians(180) + Math.atan2(leftOrRight * (6 * Params.HALF_MAT - Math.abs(shootPosY)), 6 * Params.HALF_MAT - shootPosX);
         shootPos = new Vector2d(shootPosX, shootPosY);
 
         // set up driving system
-        drive = new MecanumDrive(hardwareMap, StartPose);
-        // before it even starts, try to detect the AprilTag (for debug purposes/safety)
-//        while (!isStarted()) {
-//            sleep(10);
-//            telemetry.addLine("Initialized. Waiting for start...");
-//            detectAprilTeg();
-//            telemetry.addData("limelight", "Detected Pattern = %f", detectedPattern);
-//            telemetry.update();
-//        }
+        drive = new MecanumDrive(hardwareMap, startPose);
+
         waitForStart();
         if (opModeIsActive()) {
             run_auto();
@@ -65,62 +54,64 @@ public class AutoNear2026 extends LinearOpMode {
 
     private void run_auto() {
         motors.triggerClose();
-//        detectedPattern = 21; //set default pattern if limelight can't detect
-        // Detect April Tag while Moving backwards to detect position
-        // Then turn to shoot position angle.
-        updateProfileAccel(false);
-        Action leg1 = drive.actionBuilder(drive.localizer.getPose())
-                .afterDisp(3 * Params.HALF_MAT, new limeLightCamera()) // start detecting after moving 20 inches
-                .afterDisp(3.1 * Params.HALF_MAT, new startLauncherAction())
-                .strafeToConstantHeading(shootPos)
-                .turnTo(shootHeading)
-                .build();
-
-        // detect April Tag while moving backwards to shoot position.
-        Actions.runBlocking(leg1);
-
+        // Run the first leg of the path: move to shooting position while detecting pattern
+        updateProfileAccel(true); //TODO: Experiment with maxMode for faster movement
+        Actions.runBlocking(drive.actionBuilder(drive.localizer.getPose())
+                .afterDisp(3 * Params.HALF_MAT, new limeLightCamera()) // start limelight detection after moving 3 half mats
+                .afterDisp(3.1 * Params.HALF_MAT, new startLauncherAction()) // start launcher motor after moving 3.1 half mats
+                // TODO: Check if the following can be combined into strafeToLinearHeading to save time
+                .strafeToConstantHeading(shootPos) // move to shooting position
+                .turnTo(shootHeading) // turn to shooting direction
+                .build());
         // shoot preload artifacts
         shootArtifacts();
-        VelConstraint baseVelConstraint = (robotPose, _path, _disp) -> {
-            return 20.0;
-        };
-        // moving to detect patten row of artifacts to pick up
-        for (int pickupIndex = 0; pickupIndex < 3; pickupIndex++ ) {
+        // the following is a velocity constraint for moving to pick up artifacts
+        // TODO: See if this can be simplified AND change speed to optimal value
+        VelConstraint pickupSpeed = (robotPose, _path, _disp) -> 20.0;
+        // Loop to go through all 3 rows to pick up artifacts and shoot them
+        for (int pickupIndex = 0; pickupIndex < 3; pickupIndex++) {
             Vector2d pickupPos;
             Vector2d pickupEndPos;
-
+            // calculate pickup position and end position based on detected pattern and pickup index
             pickupPos = rowChoose((detectedPattern + pickupIndex) % 3);
-
-            pickupEndPos = new Vector2d(pickupPos.x, pickupPos.y + leftOrRight * (2 + ((pickupPos.x>0)? 0 : 1))* Params.HALF_MAT );
-
+            // fixed polarity below (there was a double negative sign before)
+            pickupEndPos = new Vector2d(
+                    pickupPos.x,
+                    pickupPos.y + (2 + ((pickupPos.x > 0) ? 0 : 1)) * Params.HALF_MAT * Math.signum(pickupPos.y)
+            );
+            // path to pick up artifacts then move to shooting position
+            // TODO: See if this code can be simplified
+            // TODO: Maybe ALWAYS take row 21 after pattern row since it takes the least time?
+            Action actIntake1;
             if ((detectedPattern + pickupIndex) % 3 > 0) {
-                Action actIntake1 = drive.actionBuilder(drive.localizer.getPose())
-                        .strafeToLinearHeading(pickupPos, Math.toRadians(90.0))
+                // after pickup, need to go back a bit to avoid obstacles from other rows
+                actIntake1 = drive.actionBuilder(drive.localizer.getPose())
+                        .strafeToLinearHeading(pickupPos, Math.toRadians(90.0*leftOrRight))
                         .afterTime(0.001, new startIntakeAction())
-                        .strafeToConstantHeading(pickupEndPos, baseVelConstraint) // picking up artifacts
+                        .strafeToConstantHeading(pickupEndPos, pickupSpeed) // picking up artifacts
                         .strafeToConstantHeading(pickupPos)
                         .afterTime(0.001, new startLauncherAction())
                         .strafeToLinearHeading(shootPos, shootHeading)
                         .build();
-                Actions.runBlocking(actIntake1);
             } else {
-                Action actIntake1 = drive.actionBuilder(drive.localizer.getPose())
-                        .strafeToLinearHeading(pickupPos, Math.toRadians(90.0))
+                // don't need to back to pickupPos after picking up artifacts in row closest to start bc no obstacles in the way
+                actIntake1 = drive.actionBuilder(drive.localizer.getPose())
+                        .strafeToLinearHeading(pickupPos, Math.toRadians(90.0*leftOrRight))
                         .afterTime(0.001, new startIntakeAction())
-                        .strafeToConstantHeading(pickupEndPos, baseVelConstraint) // picking up artifacts
+                        .strafeToConstantHeading(pickupEndPos, pickupSpeed) // picking up artifacts
                         .afterTime(0.001, new startLauncherAction())
                         .strafeToLinearHeading(shootPos, shootHeading)
                         .build();
-                Actions.runBlocking(actIntake1);
             }
-            // shoot first picked up artifacts
+            Actions.runBlocking(actIntake1);
+            // shoot picked up artifacts
             shootArtifacts();
-
-            // stop intake motor
             motors.stopIntake();
         }
     }
 
+    // function to shoot 3 artifacts
+    // TODO: Adjust timing based on actual robot performance
     private void shootArtifacts() {
         int waitTimeForTriggerClose = 300;
         int waitTimeForTriggerOpen = 950;
@@ -137,36 +128,17 @@ public class AutoNear2026 extends LinearOpMode {
         motors.triggerClose(); //close trigger to wait launcher motor speed up after first launching
 
         motors.startIntake(); // start intake motor to move 3rd artifacts into launcher
-        sleep(waitTimeForTriggerOpen);// waiting time for launcher motor ramp up
+        sleep(waitTimeForTriggerOpen); // waiting time for launcher motor ramp up
         motors.triggerOpen(); // shoot second
         sleep(waitTimeForTriggerClose);
 
         motors.triggerClose();
         sleep(waitTimeForTriggerOpen); // waiting time for launcher motor ramp up
-        motors.triggerOpen();  // shoot third
+        motors.triggerOpen(); // shoot third
         sleep(waitTimeForTriggerClose + 150);
 
         motors.triggerClose();
-        //motors.stopIntake();
         motors.stopLauncher();
-    }
-
-    private void sortArtifacts(int pattern) {
-        switch (pattern) {
-            case 21:
-                telemetry.addLine("Sorting: GPP");
-                break;
-            case 22:
-                telemetry.addLine("Sorting: PGP");
-                break;
-            case 23:
-                telemetry.addLine("Sorting: PPG");
-                break;
-            default:
-                telemetry.addLine("Sorting: Unknown");
-                break;
-        }
-        telemetry.update();
     }
 
     // function that chooses the right row based on detected pattern, returns a Vector2d
@@ -177,23 +149,26 @@ public class AutoNear2026 extends LinearOpMode {
         );
     }
 
+    // limelight detection action
     private class limeLightCamera implements Action {
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-            for (int i = 0; i < 30; i++) { // up to ~0.3s
+            for (int i = 0; i < 30; i++) { // check for 30 cycles (~30 milliseconds) to detect pattern
                 detectedPattern = patternDetector.returnId();
                 Logging.log("pattern  = %f", detectedPattern);
-                telemetry.addData("line light","Detected Pattern = %f", detectedPattern);
+                telemetry.addData("limelight", "Detected Pattern = %f", detectedPattern);
                 telemetry.update();
                 if ((detectedPattern > 20) && (detectedPattern < 24)) {
                     return false;
                 }
                 sleep(1);
             }
+            detectedPattern = 21; // default pattern if limelight can't detect
             return false;
         }
     }
 
+    // action to start launcher motor
     private class startLauncherAction implements Action {
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
@@ -203,6 +178,7 @@ public class AutoNear2026 extends LinearOpMode {
         }
     }
 
+    // action to start intake motor
     private class startIntakeAction implements Action {
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
@@ -212,28 +188,36 @@ public class AutoNear2026 extends LinearOpMode {
         }
     }
 
-    public void detectAprilTeg() {
-        for (int i = 0; i < 30; i++) { // up to ~0.3s
-            Logging.log("pattern  = %f", detectedPattern);
-            detectedPattern = patternDetector.returnId();
-            if ((detectedPattern > 20) && (detectedPattern < 24)) {
-                return;
-            }
-            sleep(1);
-        }
-        detectedPattern = 21;
-        return;
-    }
-
-    private void updateProfileAccel(boolean slowMode) {
-        if (slowMode) {
-            MecanumDrive.PARAMS.maxWheelVel = 65;
-            MecanumDrive.PARAMS.maxProfileAccel = 50;
-            MecanumDrive.PARAMS.minProfileAccel = -40;
-        } else {
+    // function to update profile acceleration for maxMode (EXPERIMENTAL)
+    // TODO: Optimize this in order to compromise accuracy and speed
+    private void updateProfileAccel(boolean maxMode) {
+        if (maxMode) {
             MecanumDrive.PARAMS.maxWheelVel = 10000;
             MecanumDrive.PARAMS.maxProfileAccel = 10000;
             MecanumDrive.PARAMS.minProfileAccel = -10000;
+        } else {
+            MecanumDrive.PARAMS.maxWheelVel = 65;
+            MecanumDrive.PARAMS.maxProfileAccel = 50;
+            MecanumDrive.PARAMS.minProfileAccel = -40;
         }
     }
+
+    // For future use when we have an artifact sorter mechanism
+    //    private void sortArtifacts(int pattern) {
+    //        switch (pattern) {
+    //            case 21:
+    //                telemetry.addLine("Sorting: GPP");
+    //                break;
+    //            case 22:
+    //                telemetry.addLine("Sorting: PGP");
+    //                break;
+    //            case 23:
+    //                telemetry.addLine("Sorting: PPG");
+    //                break;
+    //            default:
+    //                telemetry.addLine("Sorting: Unknown");
+    //                break;
+    //        }
+    //        telemetry.update();
+    //    }
 }
